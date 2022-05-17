d9148f355a5ce3e255f25df527593b1adfb12803
luke <luke.rast@gmail.com>
2022-05-16 20:32:06 -0400

    adds brute force tracking

--- setup.py
+++ setup.py
@@ -5,21 +5,23 @@ from mnistTracking import linearNetwork
 from mnistDatasets import getMNISTDatasets
 
 from torch.utils.data import DataLoader
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 if __name__ == '__main__':
-    # make dataloaders
-    trainData, validateData, testData = getMNISTDatasets()
+    #data
+    datasets = getMNISTDatasets()
+    trainDL = DataLoader(datasets[0], batch_size=10, shuffle=True, num_workers=4)
+    testDL = DataLoader(datasets[2], batch_size=len(datasets[2]))
 
-    trainDL = DataLoader(trainData, batch_size=10, shuffle=True, num_workers=4)
-    testDL = DataLoader(testData, batch_size=len(testData))
-
-    # initialize the model
+    # model
     model = linearNetwork()
 
-    trainer = pl.Trainer(max_epochs=30)
+    # tracking
+    callbacks = [ModelCheckpoint(every_n_epochs=1, save_weights_only=True, save_top_k=-1)]
 
-    # run these for trianing and testing
+    trainer = pl.Trainer(max_epochs=30, callbacks=callbacks)
+
+    # run these for training and testing
     #trainer.fit(model, trainDL)
     #trainer.test(model, testDL)
